4fb44af58bf5ad8790687784d7dfe450ce24506d
Henrik Anfinsen <henrik.anfinsen@gmail.com>
2026-07-29 12:54:54 +0200

    test(lyx): pin definition and symbol in the codeintel help tree
    Update TestHelpTree_VerbModuleSubcommands's "codeintel" entry to expect
    "refs", "definition", and "symbol" now that both new verbs are registered.

--- cmd/lyx/helptree_test.go
+++ cmd/lyx/helptree_test.go
@@ -109,7 +109,7 @@ func TestHelpTree_VerbModuleSubcommands(t *testing.T) {
 		{
 			name:     "codeintel",
 			module:   "codeintel",
-			wantSubs: []string{"refs"},
+			wantSubs: []string{"refs", "definition", "symbol"},
 		},
 		{
 			name:     "webster",
